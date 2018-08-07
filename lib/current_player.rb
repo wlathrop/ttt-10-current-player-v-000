8d9473c9c9e4bcb5519367dd78d5e7126f6c9b15
William Lathrop <wlathrop10@gmail.com>
2018-08-07 20:57:54 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  turns = 0 
+  board.each do |space|
+  if space == "X" || sapce == "O"
+    turns += 1 
+  end
+end
+return turn 
+end
+
+def current_player(board) 
+turns = turns_count(board)
+if turns % 2 = 0 
+  return "X"
+  else
+    return "O"
+  end
+end
+
+    
